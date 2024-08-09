4ab44bdd7dc8ad89e2d0c35c3145ae2720f95131
Deepak Shakya <91791+divinedragon@users.noreply.github.com>
2024-08-10 04:44:36 +0530

    Uses Pino Logger and UUID module

--- multi-agent-chat/router/index.js
+++ multi-agent-chat/router/index.js
@@ -1,128 +1,119 @@
 const express = require("express");
+const uuid = require("uuid");
+const pino = require("pino");
 
 const app = express();
+const logger = pino({});
 
 const expressWs = require("express-ws")(app);
 
 const open_connections = {};
 
-expressWs.getWss().on("connection", function (ws, req) {
-  console.log("connection open", req.query.id);
-  const connection_id = req.query.id;
-  open_connections[connection_id] = ws;
-  console.log(`Connection Successful for ClientId[${connection_id}]`);
-  console.log("Open Connections: ", Object.keys(open_connections));
-});
+
 
 // Example middleware
-app.use(function (req, res, next) {
-  console.log("middleware");
-  req.testing = "testing";
+app.use((req, res, next) => {
+  const request_id = req?.query?.id || uuid.v4();
+  logger.info("Middleware Called. Adding a Request Tracing Id - %s", request_id);
+  req.request_id = request_id;
   return next();
 });
 
-app.get("/rest-endpoint", (req, res) => {
-  console.log("Sample REST endpoint");
-  res.json({
-    message: "Response from REST endpoint",
-  });
-});
 
-app.get("/aysnc-chat-message", (req, res) => {
-  console.log("Async Chat Message -->  " + JSON.stringify(req.query));
 
+// Handle New Websocket Connections
+expressWs.getWss().on("connection", (ws, req) => {
   const connection_id = req.query.id;
-  const data = req.query.data;
-
-  console.log(`Connection Id[${connection_id}] -> Data[${data}]`);
-
-  const ws = open_connections[connection_id];
-  if (ws) {
-    console.log("Sending message to client");
-    ws.send(
-      JSON.stringify({
-        status: "active",
-        data: "Push message - " + data,
-      })
-    );
-
-    res.send("OK");
-  } else {
-    console.log("No connection found for connection_id: " + connection_id);
-    res
-      .status(404)
-      .send("No connection found for connection_id: " + connection_id);
-  }
+  open_connections[connection_id] = ws;
+  logger.info("Connection Successful for ClientId[%s]", connection_id);
+  logger.info("Open Connection List: %j", Object.keys(open_connections));
 });
 
-app.get("/aysnc-chat-terminate", (req, res) => {
-  const connection_id = req.query.id;
-  const reason = req.query.reason;
-
-  console.log(`Connection Id[${connection_id}] -> Reason[${reason}]`);
-
-  const ws = open_connections[connection_id];
-  if (ws) {
-    console.log("Sending terminate message to client");
-    ws.send(
-      JSON.stringify({
-        status: "terminated",
-        data: "Terminate Reason - " + reason,
-      })
-    );
-
-    ws.close();
-
-    delete open_connections[connection_id];
-
-    res.send("OK");
-  } else {
-    console.log("No connection found for connection_id: " + connection_id);
-    res
-      .status(404)
-      .send("No connection found for connection_id: " + connection_id);
-  }
-});
-
-app.ws("/", function (ws) {
-  ws.on("message", function (msg) {
+// Handle Incoming WebSocket Messages
+app.ws("/", (ws) => {
+  ws.on("message", (msg) => {
     const payload = JSON.parse(msg);
-
-    console.log("Socket msg --> ", msg);
-    console.log("Payload --> ", payload);
 
     const connection_id = payload.id;
     const data = payload.data;
 
-    console.log(`Connection Id: ${connection_id} - Data: ${data}`);
+    logger.info("Client[%s] Sent -> Message[%s], Data[%s]", connection_id, msg, data);
 
     if (connection_id) {
       const connected_socket = open_connections[connection_id];
       if (connected_socket) {
-        connected_socket.send(
-          JSON.stringify({
-            status: "active",
-            data: "msg received - " + data,
-          })
-        );
+        const response = JSON.stringify({ status: "active", data: "Received - " + data });
+        logger.info("Replying to Known Client[%s] - %j", connection_id, response);
+        connected_socket.send(response);
       } else {
-        console.log("msg received from unknown connection - " + connection_id);
-        ws.send(
-          JSON.stringify({
-            status: "terminated",
-          })
-        );
+        const response = JSON.stringify({ status: "terminated" });
+        logger.warn("Replying with Terminate Response to Unknown Client[%s] - %j", connection_id, response);
+        ws.send(response);
       }
     } else {
-      console.log("No Connection Id provided");
-      ws.send(
-        JSON.stringify({
-          status: "terminated",
-        })
-      );
+      const response = JSON.stringify({ status: "terminated" });
+      logger.warn("Replying with Terminate Response to Client without Id - %j", response);
+      ws.send(response);
     }
   });
 });
 
-//The server starts and listens on port 3000.
-app.listen(3000, () => console.log("Server started on http://localhost:3000"));
+
+
+// Simple Rest Endpoint (no Websocket based interaction)
+app.get("/rest-endpoint", (req, res) => {
+  logger.info("Rest Endpoint - QueryParams[%j]", req.query);
+  res.json({ message: "Response from REST endpoint" }).send();
+});
+
+
+
+// Rest Endpoint to Send a Message through existing Websocket
+app.get("/async-chat-messages", (req, res) => {
+
+  const connection_id = req.query.id;
+  const data = req.query.data;
+
+  logger.info("Async Message for Client[%s] -> QueryParams[%s], Data[%s]", connection_id, req.query, data);
+
+  const ws = open_connections[connection_id];
+  if (ws) {
+    const response = JSON.stringify({ status: "active", data: "Async - " + data });
+    logger.info("Sending Async Message to Client[%s] - %j", connection_id, response);
+    ws.send(response);
+    res.json({ success: true }).send();
+  } else {
+    logger.warn("Cannot sending Async Message to Client[%s] - No connection found with Id[%s]", connection_id, connection_id);
+    res.status(404).json({ status: false, message: "No connection found for connection_id: " + connection_id }).send();
+  }
+});
+
+// Rest Endpoint to Terminate existing Websocket
+app.get("/async-chat-termination", (req, res) => {
+  const connection_id = req.query.id;
+  const reason = req.query.reason;
+
+  logger.info("Async Termination for Client[%s] -> QueryParams[%s], Reason[%s]", connection_id, req.query, reason);
+
+  const ws = open_connections[connection_id];
+
+  if (ws) {
+
+    const response = JSON.stringify({ status: "terminated", data: "Terminate Reason - " + reason });
+    logger.info("Sending Async Terminate to Client[%s] - %j", connection_id, response);
+
+    ws.send(response);
+    ws.close();
+
+    delete open_connections[connection_id];
+
+    res.json({ success: true }).send();
+  } else {
+
+    logger.warn("Cannot sending Async Terminate to Client[%s] - No connection found with Id[%s]", connection_id, connection_id);
+    res.status(404).json({ status: false, message: "No connection found for connection_id: " + connection_id }).send();
+  }
+});
+
+// Start the Server
+app.listen(3000, () => logger.info("Server started on http://localhost:3000"));
